196b8aca2a39a656227039564e7c0c5802456212
GitHub Action <action@github.com>
2025-03-03 21:34:41 +0000

    Initialize template repository proper
    Set package name to com.github.tr4k41s.misc
    Set project name to misc
    Set modid to misc

--- src/main/java/com/github/tr4k41s/misc/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/tr4k41s/misc/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.tr4k41s.misc.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
