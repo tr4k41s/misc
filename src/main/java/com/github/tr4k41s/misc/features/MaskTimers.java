c3ba54c5ea204b27f9a664ced354870207893334
tr4k41s <tr4k41s@gmail.com>
2025-03-05 14:54:28 +0200

    code improvements

--- src/main/java/com/github/tr4k41s/misc/features/MaskTimers.java
+++ src/main/java/com/github/tr4k41s/misc/features/MaskTimers.java
@@ -16,9 +16,6 @@ import net.minecraft.util.Vec3;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import java.util.Objects;
 
 import static com.github.tr4k41s.misc.Misc.mc;
 
@@ -40,36 +37,20 @@ public class MaskTimers {
         @SubscribeEvent
         public void onChat(ClientChatReceivedEvent event) {
             if (!ScoreboardUtils.inCatacombs() || !isToggled) return;
-            EntityPlayerSP player;
             ItemStack head;
             String message = event.message.getUnformattedText();
-            if (message.contains("xd")) {
-                pop[1] = 50;
-                pop[2] = 100;
-                pop[0] = 150;
-                popped[1] = true;
-                popped[2] = true;
-                popped[0] = true;
-            } //debug
             switch (message) {
                 case "Your Bonzo's Mask saved your life!":
                 case "Your ⚚ Bonzo's Mask saved your life!":
-                    player = mc.thePlayer;
-                    head = player.getCurrentArmor(3);
-                    if (head != null && head.getItem() == Items.skull) {
-                        for (String line : Utils.getItemLore(head)) {
-                            String stripped = StringUtils.stripControlCodes(line);
-                            if (stripped.startsWith("Cooldown: ")) {
-                                bonzoCD = Integer.parseInt(stripped.replaceAll("\\D", "")) * 20;
-                                break;
-                            }
-                        }
-                        if (bonzoCD > 0) {
-                            pop[0] = bonzoCD;
-                            popped[0] = true;
-                        }
+                    head = mc.thePlayer.getCurrentArmor(3);
+                    if (head == null || head.getItem() != Items.skull) return;
+                    for (String line : Utils.getItemLore(head)) {
+                        String stripped = StringUtils.stripControlCodes(line);
+                        if (!stripped.startsWith("Cooldown: ")) continue;
+                        pop[0] = Integer.parseInt(stripped.replaceAll("\\D", "")) * 20;;
+                        popped[0] = true;
+                        break;
                     }
-                    break;
                 case "Second Wind Activated! Your Spirit Mask saved your life!":
                     pop[1] = 600;
                     popped[1] = true;
@@ -83,18 +64,15 @@ public class MaskTimers {
 
     @SubscribeEvent
     public void onPacketReceived(PacketEvent.ReceiveEvent event) {
-        if (mc.theWorld != null && mc.thePlayer != null && event.packet instanceof S32PacketConfirmTransaction) {
-            String[] names = {"Bonzo's Mask", "Spirit Mask", "Phoenix Pet"};
-            String[] sounds = {"note.pling","mob.blaze.hit","fire.ignite","random.break","Custom"};
-            String sound = MiscConfig.MaskTimerSoundValue == 4 ? MiscConfig.MaskTimerSound : sounds[MiscConfig.MaskTimerSoundValue];
-            for (int i = 0; i < pop.length; i++) {
-                if (!popped[i] || pop[i]-- > 1) continue;
-                popped[i] = false;
-                ChatUtils.sendModMessage("§b" + names[i] + "§f is now available!");
-                if (MiscConfig.MaskTimerSoundToggle) {
-                    SoundUtils.playSound(new Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ), sound, MiscConfig.MaskTimerSoundVolume, MiscConfig.MaskTimerSoundPitch);
-                }
-            }
+        if (mc.theWorld == null || mc.thePlayer == null || !(event.packet instanceof S32PacketConfirmTransaction)) return;
+        String[] names = {"Bonzo's Mask", "Spirit Mask", "Phoenix Pet"};
+        String[] sounds = {"note.pling","mob.blaze.hit","fire.ignite","random.break","Custom"};
+        String sound = MiscConfig.MaskTimerSoundValue == 4 ? MiscConfig.MaskTimerSound : sounds[MiscConfig.MaskTimerSoundValue];
+        for (int i = 0; i < pop.length; i++) {
+            if (!popped[i] || pop[i]-- > 1) continue;
+            popped[i] = false;
+            ChatUtils.sendModMessage("§b" + names[i] + "§f is now available!");
+            if (MiscConfig.MaskTimerSoundToggle) { SoundUtils.playSound(new Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ), sound, MiscConfig.MaskTimerSoundVolume, MiscConfig.MaskTimerSoundPitch); }
         }
     }
 }
